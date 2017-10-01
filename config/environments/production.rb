4a7a2b6170893e40436e4545fbfdd129d8956bfa
Breno Perucchi <bperucchi@gmail.com>
2017-10-01 01:40:16 -0300

    setup assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,11 +20,10 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
-  config.assets.css_compressor = :yui
   config.assets.js_compressor = :uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
