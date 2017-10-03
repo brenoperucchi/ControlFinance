05d51633b26e94aab67f61bd921007416258edd8
Breno Perucchi <bperucchi@gmail.com>
2017-10-02 21:10:54 -0300

    bin

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :log_level, :debug
 set :use_sudo, false
 set :bundle_binstubs, nil
 set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.4.2'
